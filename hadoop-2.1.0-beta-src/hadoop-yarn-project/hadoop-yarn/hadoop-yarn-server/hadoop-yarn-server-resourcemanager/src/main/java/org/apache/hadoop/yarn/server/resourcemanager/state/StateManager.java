780cbe89c267548b261d56bcc37157c104b067fc
Purujit Saha <purujit.saha@gmail.com>
2013-10-19 17:00:07 -0700

    Added interface declaration for state manager.

---
+++ hadoop-2.1.0-beta-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/state/StateManager.java
@@ -0,0 +1,9 @@
+package org.apache.hadoop.yarn.server.resourcemanager.state;
+
+/**
+ * ResourceManager uses this interface to manage it's knowledge about the cluster state.
+ *
+ */
+public interface StateManager {
+
+}
